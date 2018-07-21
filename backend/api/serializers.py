ef6fef842047149dde47ffa11a4ee83940f8a91c
yasser kotrsi <yasserkotrsi@gmail.com>
2018-07-21 12:15:21 +0200

    Create serializers for Layer and User

---
+++ backend/api/serializers.py
@@ -0,0 +1,22 @@
+
+from rest_framework import serializers
+from .models import User
+from .models import Layer
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = User
+        fields = ('id', 'identifier','name', 'email', 'region','date_created', 'date_modified')
+        read_only_fields = ('id', 'date_created', 'date_modified')
+
+class LayerSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Layer
+        fields = ('id', 'identifier','name', 'high', 'width','date_created', 'date_modified')
+        read_only_fields = ('id', 'date_created', 'date_modified')
