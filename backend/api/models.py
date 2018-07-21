be4e9dc2997b1a4c60b79e5b0e0ee1b9a7d727b2
yasser kotrsi <yasserkotrsi@gmail.com>
2018-07-21 12:02:40 +0200

    Database migrations done on the Layer table

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,3 +12,16 @@ class User(models.Model):
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{} {} {} {}".format(self.identifier, self.name, self.email, self.region)
+
+class Layer(models.Model):
+    """This class represents the Layer model."""
+    identifier = models.CharField(max_length=255, blank=False, unique=True)
+    name = models.CharField(max_length=255, blank=False)
+    high = models.DecimalField(max_digits=19, decimal_places=2)
+    width = models.DecimalField(max_digits=19, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{} {} {} {}".format(self.identifier, self.name, self.high, self.width)
