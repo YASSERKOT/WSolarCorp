66094e4f4f9f70bbc22899f5c357f99e7b994f51
yasser kotrsi <yasserkotrsi@gmail.com>
2018-07-21 11:46:20 +0200

    Database migrations done on the User table

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """This class represents the User model."""
+    identifier = models.CharField(max_length=255, blank=False, unique=True)
+    name = models.CharField(max_length=255, blank=False)
+    email = models.CharField(max_length=255, blank=False)
+    region = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{} {} {} {}".format(self.identifier, self.name, self.email, self.region)
