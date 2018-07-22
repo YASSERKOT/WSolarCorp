431e9b6add390d9cc2c8b92defe3aacb0e2731f5
yasser kotrsi <yasserkotrsi@gmail.com>
2018-07-22 23:57:39 +0200

    Add Leaflet map to the map

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 
-import { map } from 'rxjs/operators';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/*
 export class AppComponent {
   title = 'app';
-/*   constructor(private http: Http) {}
+   constructor(private http: Http) {}
   public getUsers() {
     const headers = new Headers();
     return this.http
            .get('http://127.0.0.1:8000/users', { headers: headers })
            .map(res => res.json().data);
   }
- */}
+}
+*/
+
+// Implémenter OnInit
+export class AppComponent implements OnInit {
+
+  // Fonction d'initialisation du composant.
+  ngOnInit() {
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Frugal Map'
+    }).addTo(myfrugalmap);
+  }
+}
